19bf8cd0d00f4d3ace8db5cc4ad5a41baee2a722
Blake Ellingham <blake.ellingham@rocksaucestudios.com>
2014-12-28 16:33:50 -0600

    finished some of section state switches

--- app/scripts/controllers/entrygrid.js
+++ app/scripts/controllers/entrygrid.js
@@ -24,4 +24,34 @@ angular.module('recommenuCmsApp')
       $scope.previewItems = function() {
          $scope.isEditingItems = false;
       };
+      $scope.addSlider = function(entry) {
+         entry.slider_templates.push({
+            'url': 'http://recommenu-test-api.herokuapp.com/slider_templates/1/', 
+            'entry': 'http://recommenu-test-api.herokuapp.com/entries/1/', 
+            'category': '', 
+            'average_score': '0', 
+            'sliders': []
+         });
+      };
+      $scope.addEntry = function() {
+         $scope.section.entries.push({
+            'id': 0, 
+            'url': '', 
+            'name': '', 
+            'section': '', 
+            'price': '', 
+            'image': '', 
+            'description': '', 
+            'star_average': '', 
+            'five_agg': 0, 
+            'four_agg': 0, 
+            'three_agg': 0, 
+            'two_agg': 0, 
+            'one_agg': 0, 
+            'top_comment': '', 
+            'review_count': 0, 
+            'hosted': true, 
+            'slider_templates': []
+         });
+      };
    });
--- app/scripts/services/section.js
+++ app/scripts/services/section.js
@@ -16,7 +16,7 @@ angular.module('recommenuCmsApp')
       return {
          creatingSection: false, 
          activeSection: null,
-         activeSectionTemplate: 'regular', 
+         activeSectionTemplate: 'custom', 
          // NEED TO ADD prices when they are relevant   
          postNewSection: function(title, description, menu) {
             var newSection = {
